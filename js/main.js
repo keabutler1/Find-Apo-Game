e8810441a358e625f4ca0b0627ee90c694cca070
Kea <keabutler1@gmail.com>
2018-05-21 16:38:53 -0700

    Firtst J'Query

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("Find Apo git stGame is Ready!")
+var timer = $("timer");
+var startButton = $("#startButton");
+var playerOneScore = $("playerOneScore")
+var playerTwoScore =$("playerOneScore")
+var img = $("Apo")
+
+// As a user, after I click the start button, the timer should start going down from 60 to zero in one second intervals.
+
+console.log("Find Apo Game is Ready!")
